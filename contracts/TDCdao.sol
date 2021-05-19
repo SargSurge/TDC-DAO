e708ddb55e21aee29fa90f1c205fd57a50fd9cbb
Sergio Perez <saperez@mit.edu>
2021-05-18 21:21:04 -0400

    added voting

--- contracts/TDCdao.sol
+++ contracts/TDCdao.sol
@@ -7,13 +7,21 @@ contract TDCdao {
     int motionCount = -1;
     mapping (address => string) private brothers;
     mapping (int => Motion) private motions;
-    
+
     struct Motion {
         string creator;
         address creatorAddress;
         string motionBody;
         bool passed;
         bool discussed;
+        bool openToVote;
+        Vote[] votes;
+    }
+
+    struct Vote {
+        string voter;
+        address voterAddress;
+        bool pass;
     }
 
     function proposeMotion(string memory _motionBody) public {
@@ -27,7 +35,7 @@ contract TDCdao {
         newMotion.motionBody = _motionBody;
         newMotion.passed = false;
         newMotion.discussed = false;
-    
+        newMotion.openToVote = false;
         }
 
     function discussedMotion(int motionID) public {
@@ -35,10 +43,73 @@ contract TDCdao {
         require (motionID <= motionCount);
         motions[motionID].discussed = true;
     }
+
+    function motionToVote(int motionID) public {
+        require (motionID >= 0);
+        require (motionID <= motionCount);
+        require (motions[motionID].discussed == true);
+        motions[motionID].openToVote = true;
+    }
+
+    function castVote(int motionID, bool vote) public {
+        require (motionID >= 0);
+        require (motionID <= motionCount);
+
+        string storage _voter = brothers[msg.sender];
+        Vote memory newVote = Vote(_voter, msg.sender, vote);
+        motions[motionID].votes.push(newVote);
+    }
+
+    function endVoting(int motionID) public {
+        require (motionID >= 0);
+        require (motionID <= motionCount);
+
+        Motion storage motion = motions[motionID];
+        if (motion.openToVote) {
+            motion.passed = _getVoteResult(motion.votes);
+            motion.openToVote = false;
+        }
+    }
+
+    function _getVoteResult(Vote[] storage votes) private view returns (bool result) {
+        int passVotes = 0;
+        int rejectVotes = 0;
+
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i].pass) {
+                passVotes++;
+            } else {
+                rejectVotes++;
+            }
+        }
+        return passVotes > rejectVotes;
+    }
     
     function getMotion(int motionID) public view returns (Motion memory motion){
         require (motionID >= 0);
         require (motionID <= motionCount);
         return motions[motionID];
     }
-}                  
+
+    function getNumMotions() public view returns (int num) {
+        return motionCount;
+    }
+}
+
+
+// TODO making motions to be their own contract
+// using a struct was limiting, was not able to limit votes on a motion to one per address.
+contract MotionContract {
+    address creatorAddress;
+    string motionBody;
+    bool passed;
+    bool discussed;
+    bool openToVote;
+    Vote[] votes;
+
+    struct Vote {
+        string voter;
+        address voterAddress;
+        bool pass;
+    }
+}
